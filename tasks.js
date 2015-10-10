a1bc54cfc692e7380998f34fe73433fd6a81dc48
Jesse Hattabaugh <arkanciscan@gmail.com>
2015-10-10 16:21:37 -0700

    old changes
    wrapping postcss in a try catch to make the build script better

--- tasks.js
+++ tasks.js
@@ -5,31 +5,37 @@ exports.css = css;
 function css (){
 	console.info('css build task started');
 	var postcss = require('postcss');
-	postcss([
-		require('postcss-inline-comment')(),
-		require('postcss-mixins')({
-			mixins: {
-				exhibits: require('./src/exhibits')
-			}
-		}),
-		require('postcss-simple-vars'),
-		require('postcss-nested'),
-	])
-		.process(fs.readFileSync(config.cssSrc, "utf8"), {
-			from: config.cssSrc,
-			to: config.cssDest
-		})
-			.then(
-				function (res) {
-					fs.writeFileSync(config.cssDest, res.css);
-					//if (res.map)
-						//fs.writeFileSync(config.cssDest +'.map', res.map);
-					console.info('css built');
+	try {
+		postcss([
+			require('postcss-inline-comment')(),
+			require('postcss-mixins')({
+				mixins: {
+					exhibits: require('./src/exhibits')
 				}
-			)
-			.catch(function (e) {
-				console.error(e);
-			});
+			}),
+			require('postcss-simple-vars'),
+			require('postcss-nested'),
+		])
+			.process(fs.readFileSync(config.cssSrc, 'utf8'), {
+				from: config.cssSrc,
+				to: config.cssDest
+			})
+				.then(
+					function (res) {
+						fs.writeFileSync(config.cssDest, res.css);
+						//if (res.map)
+							//fs.writeFileSync(config.cssDest +'.map', res.map);
+						console.info('css built');
+					}
+				)
+				.catch(function (e) {
+					console.error('css build failed');
+					//console.error(e);
+				});
+	} catch(e) {
+		console.dir(e);
+	}
+
 }
 
 exports.html = html;
