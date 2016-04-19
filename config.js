0c7655e8fdfe23c5696548790aed33ee8e01b353
Jesse Hattabaugh <arkanciscan@gmail.com>
2016-04-19 21:25:57 +0000

    stuff

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var path = require('path');
 exports.src = 'src';
 exports.cssSrc = path.join(exports.src, 'main.scss');
 exports.cssDest = 'index.css';
-exports.htmlSrc = path.join(exports.src, 'main.pug');
+exports.htmlSrc = path.join(exports.src, 'main.jade');
 exports.htmlDest = 'index.html';
 exports.min = process.env.NODE_ENV === 'production';
 exports.port = process.env.PORT || 3000;
